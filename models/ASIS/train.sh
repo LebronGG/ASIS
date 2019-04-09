400740956d31a606afb9d86a736349dc195f3b40
LebronGG <38722951+LebronGG@users.noreply.github.com>
2019-04-09 19:53:15 +0800

    Update train.sh

--- models/ASIS/train.sh
+++ models/ASIS/train.sh
@@ -1,5 +1,5 @@
 i=1
-python train.py --gpu 0 --batch_size 8 --max_epoch 100  --log_dir log${i} --learning_rate 0.001 --decay_step 300000  --restore_model None --input_list data/train_hdf5_file_list_woArea${i}.txt
+#python train.py --gpu 0 --batch_size 8 --max_epoch 100  --log_dir log${i} --learning_rate 0.001 --decay_step 300000  --restore_model None --input_list data/train_hdf5_file_list_woArea${i}.txt
 python estimate_mean_ins_size.py --test_area ${i}
-python test.py --gpu 0 --bandwidth 0.6 --log_dir log${i}_test --model_path log${i}/epoch_99.ckpt --input_list  meta/area${i}_data_label.txt --verbose  
-python eval_iou_accuracy.py
+python test.py --gpu 0 --bandwidth 0.6 --log_dir log${i}_test --model_path log${i}/epoch_10.ckpt --input_list  meta/area${i}_data_label.txt --verbose
+python eval_iou_accuracy.py --test_area ${i}
